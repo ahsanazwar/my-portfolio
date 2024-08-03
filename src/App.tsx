4dbeb98b2cec7e3afbe691ace64df68e2061f895
ahsanazwar <ahsan.azwar@gmail.com>
2024-08-04 00:11:52 +0500

    update loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import Career from './components/career'
 import Education from './components/education'
 import MainBanner from './components/main-banner'
 import Projects from './components/projects'
+import WebLoader from './components/web-loader'
 
 function App() {
   // Hello
   return (
     <>
       {/* <img src="dark-logo.png" width={250} height={50} alt="Logo" /> */}
-      <main className="bg-black pt-10">
+      <main className="bg-black pt-10"> 
+        <WebLoader/>
         <MainBanner/>
         <Projects/>
         <Education/>
---
+++ src/components/web-loader.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react";
+
+
+
+const WebLoader = () => {
+
+    const loader = useRef<any>();
+
+    useEffect(()=>{
+        
+
+        setTimeout(() => {
+            loader.current.style.transform = "scaleX(0)";
+        }, 2000)
+
+    })
+    return ( 
+        <>
+            <div ref={loader} className="customLoader flex h-screen fixed w-full top-0 left-0 z-50 bg-white items-center justify-center">
+                <img src="infinite-spinner.svg"  width="70px" height="70px"/> <p className="text-purple text-[35px]">&nbsp;&nbsp; Loading...</p>
+            </div>
+        </>
+     );
+}
+ 
+export default WebLoader;
