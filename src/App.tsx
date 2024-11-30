26245f83ac2a11935093707b9bf2ac8819c3930a
ahsanazwar <ahsan.azwar@gmail.com>
2024-11-30 19:03:38 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -31,20 +31,20 @@ function App() {
 
 
   useEffect(() => {
-      gsap.to('.custom-pattern', {
-        y: -60, 
-        scrollTrigger: {
-          trigger: '.custom-pattern',
-          start: "bottom bottom",
-          scrub: 2,
-          snap: {
-            snapTo: 'labels', // snap to the closest label in the timeline
-            duration:2.5, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
-            delay: 0.1, // wait 0.2 seconds from the last scroll event before doing the snapping
-            // ease: "power1.out" // the ease of the snap animation ("power3" by default)
-        }
-        }
-      });
+      // gsap.to('.custom-pattern', {
+      //   y: -60, 
+      //   scrollTrigger: {
+      //     trigger: '.custom-pattern',
+      //     start: "bottom bottom",
+      //     scrub: 2,
+      //     snap: {
+      //       snapTo: 'labels', // snap to the closest label in the timeline
+      //       duration:2.5, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+      //       delay: 0.1, // wait 0.2 seconds from the last scroll event before doing the snapping
+      //       // ease: "power1.out" // the ease of the snap animation ("power3" by default)
+      //   }
+      //   }
+      // });
 
       gsap.set('.follower',{xPercent:-50,yPercent:-50});
       gsap.set('.cursor',{xPercent:-50,yPercent:-50});
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from '@App.tsx'
 import '@index.css';
 import ThemeState from '@context/theme/themeState';
+// import { Scrollbars } from 'react-custom-scrollbars-2';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeState>
-      <App />  
-    </ThemeState>
+    {/* <Scrollbars 
+      thumbMinSize={50}
+      universal={true}
+      renderThumbVertical={() => <div className="thumb-vertical"/>}
+    > */}
+      <ThemeState>
+        <App />    
+      </ThemeState>
+    {/* </Scrollbars> */}
   </React.StrictMode>,
 )
